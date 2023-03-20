ead1a915a65050ebce01177e04aa8dc284c28855
Iliyana Pantaleeva <lia.pantaleeva@gmail.com>
2023-03-20 22:29:50 +0200

    add configuration change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/platform_o/',
+  // base: '/platform_o/',
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
 })
